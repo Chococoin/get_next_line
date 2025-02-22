616897cf08643e55c33cf2fe2488ff9981616801
German G. Lugo Munoz <glugo-mu@42firenze.it>
2025-02-22 07:41:07 +0100

    Fix memory leaks.

--- get_next_line.c
+++ get_next_line.c
@@ -6,27 +6,11 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 12:18:42 by glugo-mu          #+#    #+#             */
-/*   Updated: 2025/02/15 12:35:32 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2025/02/22 07:30:15 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-# ifdef DEBUG_MEMORY
-
-void *debug_malloc(size_t size, const char *file, int line)
-{
-    void *ptr = __builtin_malloc(size);
-    printf("[MALLOC] %p (%zu bytes) at %s:%d\n", ptr, size, file, line);
-    return ptr;
-}
-
-void debug_free(void *ptr, const char *file, int line)
-{
-    printf("[FREE] %p at %s:%d\n", ptr, file, line);
-    __builtin_free(ptr);
-}
-# endif
 
 void	add_to_store(char **store, char *buffer, int start)
 {
@@ -54,32 +38,25 @@ void	add_to_store(char **store, char *buffer, int start)
 	}
 }
 
-void	fill_line(char *buffer, char **line, char **store)
+void	handle_eof(char **line, char **store)
 {
-	int		store_len;
-	int		buffer_len;
-	char	*new_line;
-
-	store_len = 0;
-	if (*store)
-		store_len = ft_strlen(*store);
-	buffer_len = 0;
-	while (buffer[buffer_len] && buffer[buffer_len] != '\n')
-		buffer_len++;
-	new_line = malloc(sizeof(char) * (store_len + buffer_len + 2));
-	if (!new_line)
-		return ;
-	ft_memcpy(new_line, *store, store_len);
-	ft_memcpy(new_line + store_len, buffer, buffer_len);
-	if (*store)
+	if (*store && **store)
+	{
+		*line = ft_strdup(*store);
+		if (!line)
+			return ;
 		free(*store);
-	*store = NULL;
-	if (buffer[buffer_len] == '\n')
-		new_line[store_len + buffer_len++] = '\n';
-	new_line[store_len + buffer_len] = '\0';
-	*line = new_line;
-	if (buffer[buffer_len])
-		add_to_store(store, buffer + buffer_len, 0);
+		*store = NULL;
+	}
+	else
+	{
+		*line = NULL;
+		if (*store)
+		{
+			free(*store);
+			*store = NULL;
+		}
+	}
 }
 
 void	get_from_store(char **store, char **line)
@@ -97,6 +74,8 @@ void	get_from_store(char **store, char **line)
 		ft_memcpy(*line, *store, len);
 		(*line)[len] = '\0';
 		ptr = ft_strdup(*store + len);
+		if (!ptr)
+			return ;
 		free(*store);
 		*store = ptr;
 	}
@@ -114,19 +93,22 @@ void	get_from_read(int fd, char **line, char **store)
 		byteslen = read(fd, buffer, BUFFER_SIZE);
 		if (byteslen <= 0)
 		{
-			if (*store && **store)
-			{
-				*line = ft_strdup(*store);
-				free(*store);
-				*store = NULL;
-			}
+			handle_eof(line, store);
 			return ;
 		}
 		buffer[byteslen] = '\0';
 		add_to_store(store, buffer, 0);
 	}
-	if (ft_strchr(*store, '\n'))
+	if (*store && ft_strchr(*store, '\n'))
 		get_from_store(store, line);
+	else if (*store)
+	{
+		*line = ft_strdup(*store);
+		if (!line)
+			return ;
+		free(*store);
+		*store = NULL;
+	}
 }
 
 char	*get_next_line(int fd)
@@ -141,12 +123,15 @@ char	*get_next_line(int fd)
 		get_from_read(fd, &line, &store);
 	else
 		get_from_store(&store, &line);
-	if (line)
-		return (line);
-	return (NULL);
+	if (!line && store)
+	{
+		free(store);
+		store = NULL;
+	}
+	return (line);
 }
 
-int main(void)
+int	main(void)
 {
 	int		fd;
 	char	*line;
@@ -157,8 +142,9 @@ int main(void)
 	while ((line = get_next_line(fd)) != NULL)
 	{
 		printf("%s", line);
-		// free(line);
+		free(line);
 	}
+	printf("%s", line);
 	close(fd);
 	return (0);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 12:19:11 by glugo-mu          #+#    #+#             */
-/*   Updated: 2025/02/16 13:14:31 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2025/02/22 07:13:01 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,16 +56,17 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-char *ft_strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-    char *dup;
-    size_t len = ft_strlen(s) + 1;
+	char	*dup;
+	size_t	len;	
 
-    dup = malloc(len);
-    if (!dup)
-        return (NULL);
-    ft_memcpy(dup, s, len);
-    return (dup);
+	len = ft_strlen(s) + 1;
+	dup = malloc(len);
+	if (!dup)
+		return (NULL);
+	ft_memcpy(dup, s, len);
+	return (dup);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
@@ -73,7 +74,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len1;
 	size_t	len2;
 	char	*result;
-	size_t	i, j;
 
 	if (!s1 && !s2)
 		return (NULL);
@@ -86,18 +86,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	result = (char *)malloc(len1 + len2 + 1);
 	if (!result)
 		return (NULL);
-	i = 0;
-	while (i < len1)
-	{
-		result[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (j < len2)
-	{
-		result[i + j] = s2[j];
-		j++;
-	}
-	result[i + j] = '\0';
+	ft_memcpy(result, s1, len1);
+	ft_memcpy(result + len1, s2, len2);
+	result[len1 + len2] = '\0';
 	return (result);
 }
